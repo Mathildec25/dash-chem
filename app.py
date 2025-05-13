7200ee41079de5e7db362e0d29b7c7d2c5eca0e9
Mathilde Croissant <mathilde.croissant25@gmail.com>
2025-05-13 15:35:59 +0200

    app se connecte au bon port

--- app.py
+++ app.py
@@ -60,4 +60,5 @@ app.layout = dbc.Container([
 
 # Launch the app
 if __name__ == '__main__':
-    app.run(debug=True)    # Allow to update the app without restarting it (debug mode)
+    app.run(host="0.0.0.0", port=8080, debug=True)
+  # Allow to update the app without restarting it (debug mode)
